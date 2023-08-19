92bb1eacba010103086f8ff6f6e6c5c60522b990
Madm4rtigan <45667497+Madm4rtigan@users.noreply.github.com>
2023-08-19 08:34:01 -0700

    Setup grid input header

--- src/components/GridGenerator.vue
+++ src/components/GridGenerator.vue
@@ -9,19 +9,27 @@ const count = ref(0);
 </script>
 
 <template>
-  <div class="text-5xl fw600">Grid Generator</div>
-  <div class="bg-white p-10 rounded drop-shadow-lg">
-    <div>
-      <span>Generate</span>
-      <input class="border-1 rounded" />
-      <span> random grids, each with </span>
+  <div class="text-5xl fw600 mb-10px">Grid Generator</div>
+  <div class="bg-white p-10px w-612px rounded drop-shadow-lg flex flex-row">
+    <div class="mt-3 mb-3 min-w-250px">
+      <span>Generate </span>
+      <input class="border-1 rounded h-38px w-56px" />
+      <span> random grids, </span>
     </div>
-    <div>
-      <input class="border-1 rounded" />
+    <div class="mt-3 mb-3 min-w-250px">
+      <span>each with </span>
+      <input class="border-1 rounded h-38px w-56px" />
       <span> rows/columns.</span>
     </div>
-   
-    <button type="button" @click="count++">Generate</button>
+    <div class="mt-3 mb-3">
+      <button
+        type="button"
+        @click="count++"
+        class="bg-blue rounded p-2 c-white fw600 px-13px py-7px"
+      >
+        Generate
+      </button>
+    </div>
   </div>
 </template>
 
